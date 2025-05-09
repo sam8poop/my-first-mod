b7896e3c5134124216f4530cc361f4b79cba2ac5
chase <sam8poop@gmail.com>
2025-05-10 02:08:22 +0930

    i did stuf

--- src/main/java/net/chase/myfirstmod/block/Modblocks.java
+++ src/main/java/net/chase/myfirstmod/block/Modblocks.java
@@ -6,7 +6,6 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
